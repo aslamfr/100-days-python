fd05e99069d73f8407adb1ab7eb7e72912223416
aslamfr <afafathin@gmail.com>
2025-06-11 11:47:14 +0700

    day 4 progressing

---
+++ day_4/coin_flip.py
@@ -0,0 +1,8 @@
+import random
+
+flip = random.randint(0,1)
+print("Start coin flip")
+if flip == 0:
+  print("Head")
+else:
+  print("Tail")
---
+++ day_4/test1.py
@@ -0,0 +1,8 @@
+import random
+import my_module
+
+random_int = random.randint(0,2)
+random_float = random.uniform(0,2)
+print(f"random integer = {random_int}")
+print(f"random float = {random_float}")
+print(f"my favourite number = {my_module.number_fav}")
